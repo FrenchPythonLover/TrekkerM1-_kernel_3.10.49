e5298d78a8c8b4155e2d2fcfacd5b9ed36053f03
FrenchPythonLover <117096932+FrenchPythonLover@users.noreply.github.com>
2026-04-09 18:02:47 +0200

    FrenchPython Update to use Python 3 and improve syntax

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -1,34 +1,5 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-# Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of The Linux Foundation nor
-#       the names of its contributors may be used to endorse or promote
-#       products derived from this software without specific prior written
-#       permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Invoke gcc, looking for warnings, and causing a failure if there are
-# non-whitelisted warnings.
 
 import errno
 import re
@@ -36,10 +7,7 @@ import os
 import sys
 import subprocess
 
-# Note that gcc uses unicode, which may depend on the locale.  TODO:
-# force LANG to be set to en_US.UTF-8 to get consistent warnings.
-
-allowed_warnings = set([
+allowed_warnings = {
     "return_address.c:63",
     "kprobes.c:1493",
     "rcutree.c:1614",
@@ -48,55 +16,65 @@ allowed_warnings = set([
     "jhash.h:137",
     "cmpxchg.h:162",
     "ping.c:87",
- ])
+}
 
-# Capture the name of the object file, can find it.
 ofile = None
 
 warning_re = re.compile(r'''(.*/|)([^/]+\.[a-z]+:\d+):(\d+:)? warning:''')
+
 def interpret_warning(line):
-    """Decode the message from gcc.  The messages we care about have a filename, and a warning"""
+    """Decode gcc warnings and fail if not allowed"""
+    global ofile
+
     line = line.rstrip('\n')
     m = warning_re.match(line)
-    if m and m.group(2) not in allowed_warnings:
-        print "error, forbidden warning:", m.group(2)
 
-        # If there is a warning, remove any object if it exists.
+    if m and m.group(2) not in allowed_warnings:
+        print("error, forbidden warning:", m.group(2))
+
         if ofile:
             try:
                 os.remove(ofile)
             except OSError:
                 pass
+
         sys.exit(1)
 
+
 def run_gcc():
+    global ofile
+
     args = sys.argv[1:]
-    # Look for -o
+
     try:
         i = args.index('-o')
-        global ofile
-        ofile = args[i+1]
+        ofile = args[i + 1]
     except (ValueError, IndexError):
         pass
 
-    compiler = sys.argv[0]
-
     try:
-        proc = subprocess.Popen(args, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(
+            args,
+            stderr=subprocess.PIPE,
+            text=True  # IMPORTANT: decode bytes → str automatiquement
+        )
+
         for line in proc.stderr:
-            print line,
+            print(line, end='')  # équivalent de print line,
             interpret_warning(line)
 
         result = proc.wait()
+
     except OSError as e:
         result = e.errno
         if result == errno.ENOENT:
-            print args[0] + ':',e.strerror
-            print 'Is your PATH set correctly?'
+            print(f"{args[0]}: {e.strerror}")
+            print("Is your PATH set correctly?")
         else:
-            print ' '.join(args), str(e)
+            print(' '.join(args), str(e))
 
     return result
+
 
 if __name__ == '__main__':
     status = run_gcc()
